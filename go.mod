0c6978657d687dcec1765c1fcfead77d307aaf7b
Tanwarat-Nat <tanwarat.nat@gmail.com>
2021-05-27 22:38:24 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/Tanwarat-Nat/todoapp
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.SetFlags(log.Lmicroseconds)
+	http.HandleFunc("/todos", todosHandler)
+	http.HandleFunc("/todos/new", newHandler)
+	http.HandleFunc("/todos/create", createHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func todosHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("index.html", "todos.html"))
+	t.Execute(w, readLines("todos.txt"))
+
+}
+
+func newHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("index.html", "new.html"))
+	t.Execute(w, nil)
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.FormValue("item"))
+	f, err := os.OpenFile("todos.txt", os.O_APPEND, os.ModeAppend)
+	Check(err)
+	_, err = fmt.Fprintln(f, r.FormValue("item"))
+	Check(err)
+	http.Redirect(w, r, "/todos", http.StatusFound)
+
+}
+
+func Check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readLines(name string) []string {
+	f, err := os.Open(name)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	Check(err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	Check(scanner.Err())
+	return lines
+}
